ca4d76be64514ddd1a2c81026600811bea2b4609
Kauã Leal Marcondes dos Santos <130408422+KauaLealz@users.noreply.github.com>
2025-01-23 16:24:47 -0300

    Configuração de ambiente

--- src/app/services/emailjs-service/emailjs.service.ts
+++ src/app/services/emailjs-service/emailjs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
-import { environment } from '../../../environments/environment.default';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
